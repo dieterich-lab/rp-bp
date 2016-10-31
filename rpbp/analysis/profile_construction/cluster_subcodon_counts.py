1c4ba2373757a6815c60af6e2dc9a077b9d20170
Brandon Malone <bmmalone@gmail.com>
2016-10-31 21:17:35 +0100

    FIX added top k% option for clustering subcodons

--- rpbp/analysis/profile_construction/cluster_subcodon_counts.py
+++ rpbp/analysis/profile_construction/cluster_subcodon_counts.py
@@ -25,6 +25,8 @@ default_n_components = 100
 default_seed=8675309
 default_min_weight = 0.01
 
+default_fraction = 0.1
+
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description="This script clusters the ORFs based on their subcodon "
@@ -33,9 +35,12 @@ def main():
         "of the list are the means, and the second is the weights.")
 
     parser.add_argument('bf', help="The bayes factor file containing counts")
-    parser.add_argument('out', help="The output (image) file")
+    parser.add_argument('out', help="The output (pickle) file")
 
-    parser.add_argument('--title', help="The title for the image")
+    parser.add_argument('--fraction', help="The top <fraction> genes, based "
+        "on normalized read counts, will be used for clustering", type=float,
+        default=default_fraction)
+
     parser.add_argument('--max-iter', help="The maximum number of iterations "
         "for clustering", type=int, default=default_max_iter)
     parser.add_argument('--n-components', help="The maximum number of "
@@ -51,11 +56,23 @@ def main():
     logger.info(msg)
     bf = bed_utils.read_bed(args.bf)
 
+    msg = "Extracting top k% of ORFs"
+    logger.info(msg)
+    
+    # calculate the normalized read coverage
+    total_read_coverage = bf["x_1_sum"] + bf["x_2_sum"] + bf["x_3_sum"]
+    rpk = total_read_coverage / bf['orf_len']
+    sorted_rpk_indices = np.argsort(rpk)
+
+    # and get the best args.fraction of them
+    num_orfs = int(len(rpk) * args.fraction)
+    top_k_orfs = sorted_rpk_indices.tail(num_orfs)
+
     msg = "Finding subcodon clusters"
     logger.info(msg)
 
     x_i_fields = ["x_1_sum", "x_2_sum", "x_3_sum"]
-    X = bf[x_i_fields]
+    X = bf.iloc[top_k_orfs.values][x_i_fields]
 
     model = np_utils.fit_bayesian_gaussian_mixture(X, 
         max_iter=args.max_iter, n_components=args.n_components, seed=args.seed)
