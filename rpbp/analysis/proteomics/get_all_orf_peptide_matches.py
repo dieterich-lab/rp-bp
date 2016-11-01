dcba047e422f29591bc6a652757af6bd060b1c3f
Brandon Malone <bmmalone@gmail.com>
2016-11-01 19:28:08 +0100

    FIX peptide matches driver uses cell types and peptides specified in config

--- rpbp/analysis/proteomics/get_all_orf_peptide_matches.py
+++ rpbp/analysis/proteomics/get_all_orf_peptide_matches.py
@@ -22,18 +22,20 @@ def main():
         description="This script identifies the orf peptide matches for all samples in "
         "a project.")
     parser.add_argument('config', help="The (yaml) config file")
-    parser.add_argument('peptides', help="The peptides.txt file produced by MaxQuant")
     
-    parser.add_argument('--peptide-filter-field', help="The field to use for filtering "
-        "the peptides from MaxQuant", default=default_peptide_filter_field)
-    parser.add_argument('--peptide-filter-value', help="All peptides with a value greater "
-        "than the filter value will be removed", type=float, default=default_peptide_filter_value)
+    parser.add_argument('--peptide-filter-field', help="The field to use for "
+        "filtering the peptides from MaxQuant", default=default_peptide_filter_field)
 
-    parser.add_argument('--peptide-separator', help="The separator in the --peptide file",
-        default=default_peptide_separator)
+    parser.add_argument('--peptide-filter-value', help="All peptides with a value "
+        "greater than the filter value will be removed", type=float, 
+        default=default_peptide_filter_value)
 
-    parser.add_argument('--note', help="If this option is given, it will be used in the "
-        "filenames.\n\nN.B. This REPLACES the note in the config file.", default=default_note)
+    parser.add_argument('--peptide-separator', help="The separator in the "
+        "peptide file", default=default_peptide_separator)
+
+    parser.add_argument('--note', help="If this option is given, it will be used in "
+        "the output filenames.\n\nN.B. This REPLACES the note in the config file.", 
+        default=default_note)
 
     slurm.add_sbatch_options(parser)
     utils.add_logging_options(parser)
@@ -51,7 +53,10 @@ def main():
     utils.check_programs_exist(programs)
 
     required_keys = [
-        'riboseq_data'
+        'peptide_files',
+        'peptide_cell_type_analysis',
+        'riboseq_data',
+        'riboseq_samples'
     ]
     utils.check_keys_exist(config, required_keys)
 
@@ -67,50 +72,50 @@ def main():
     peptide_filter_value_str = utils.get_config_argument(args_dict, 'peptides_filter_value')
     peptide_separator_str = utils.get_config_argument(args_dict, 'peptide_separator')
 
-    num_procs_str = utils.get_config_argument(args_dict, 'num_cpus', 'num-procs')
-    
-    for name, data in config['riboseq_samples'].items():
-        msg = "Sample: {}".format(name)
-        logging.debug(msg)
+    num_cpus_str = utils.get_config_argument(args_dict, 'num_cpus')
 
-        try:
-            lengths, offsets = riboutils.ribo_utils.get_periodic_lengths_and_offsets(config, name, args.do_not_call)
-        except FileNotFoundError:
-            msg = "Could not parse out lengths and offsets for sample: {}. Skipping".format(name)
-            logging.error(msg)
+    cell_types = ribo_utils.get_riboseq_cell_type_samples(config)
+    for cell_type, peptide_files in config['peptide_cell_type_analysis'].items():
+        if cell_type not in cell_types:
+            msg = ("Could not find cell_type specification. Please check the config "
+                "file: {}".format(cell_type))
+            logger.warning(msg)
             continue
-        
-        ### bf predictions
+            
+        cell_type_protein = ribo_filenames.get_riboseq_cell_type_protein(
+            config['riboseq_data'], cell_type, is_filtered=True, note=config['note'])
 
-        predicted_proteins = filenames.get_riboseq_predicted_orfs_protein(
-            config['riboseq_data'], name, length=lengths, offset=offsets, 
-            is_unique=True, note=note_str)
+        if not os.path.exists(cell_type_protein):
+            msg = ("Could not find cell_type protein fasta. Skipping: {}".
+                format(cell_type_protein))
+            logger.warning(msg)
+            continue
+            
+        for peptide_file in peptide_files:
+            if peptide_file not in config['peptide_files']:
+                msg = ("Could not find peptide_file specification. Please check "
+                    "the config file: {}".format(peptide_file))
+                logger.warning(msg)
+                continue
+                
+            peptide_txt_file = config['peptide_files'][peptide_file]
+                
+            if not os.path.exists(peptide_txt_file):
+                msg = ("Could not find peptide.txt file. Skipping: {}".
+                    format(peptide_txt_file))
+                logger.warning(msg)
+                continue
+                
+            peptide_matches = ribo_filenames.get_riboseq_peptide_matches(
+                config['riboseq_data'], cell_type, peptide_file, 
+                is_filtered=True, note=out_note_str)
+                
+            cmd = "get-orf-peptide-matches {} {} {} {} {} {} {} {}".format(cell_type_protein, 
+                peptide_txt_file, peptide_matches, num_cpus_str, peptide_filter_field_str, 
+                peptide_filter_value_str, peptide_separator_str, logging_str)
 
-        peptide_matches = filenames.get_riboseq_peptide_matches(
-            config['riboseq_data'], name, length=lengths, offset=offsets, 
-            is_unique=True, note=out_note_str)
+            slurm.check_sbatch(cmd, args=args)
 
-        cmd = "get-orf-peptide-matches {} {} {} {} {} {} {} {}".format(predicted_proteins, 
-            args.peptides, peptide_matches, num_procs_str, peptide_filter_field_str, 
-            peptide_filter_value_str, peptide_separator_str, logging_str)
-
-        slurm.check_sbatch(cmd, args=args)
-
-        ### chisq predictions
-
-        predicted_chisq_proteins = filenames.get_riboseq_predicted_orfs_protein(
-            config['riboseq_data'], name, length=lengths, offset=offsets, 
-            is_unique=True, note=note_str, is_chisq=True)
-
-        chisq_peptide_matches = filenames.get_riboseq_peptide_matches(
-            config['riboseq_data'], name, length=lengths, offset=offsets, 
-            is_unique=True, note=out_note_str, is_chisq=True)
-
-        cmd = "get-orf-peptide-matches {} {} {} {} {} {} {} {}".format(predicted_chisq_proteins, 
-            args.peptides, chisq_peptide_matches, num_procs_str, peptide_filter_field_str, 
-            peptide_filter_value_str, peptide_separator_str, logging_str)
-
-        slurm.check_sbatch(cmd, args=args)
-
+           
 if __name__ == '__main__':
     main()
