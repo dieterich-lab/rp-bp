a7420e5f61394fb295388623b481a700eada197c
Brandon Malone <bmmalone@gmail.com>
2016-11-13 17:01:47 +0100

    FIX renamed process-all-samples to run-all-rpbp-instances

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ profile_construction_scripts = [
     'smooth-orf-profiles=rpbp.orf_profile_construction.smooth_orf_profiles:main',
     'merge-replicate-orf-profiles=rpbp.translation_prediction.merge_replicate_orf_profiles:main',
     'run-rpbp-pipeline=rpbp.run_rpbp_pipeline:main',
-    'process-all-samples=rpbp.process_all_samples:main',
+    'run-all-rpbp-instances=rpbp.run_all_rpbp_instances:main',
 ]
 
 translation_prediction_scripts = [
