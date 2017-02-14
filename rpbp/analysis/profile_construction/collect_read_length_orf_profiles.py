0f964c71481ff4153ac8eb95540dd3196a58048c
Brandon Malone <bmmalone@gmail.com>
2017-02-14 11:46:37 +0100

    UPD use config for read-length specific ORF profile files
    This is in relation to Issue #49.

--- rpbp/analysis/profile_construction/collect_read_length_orf_profiles.py
+++ rpbp/analysis/profile_construction/collect_read_length_orf_profiles.py
@@ -4,6 +4,12 @@ import argparse
 import gzip
 import string
 import scipy.io
+import yaml
+
+import misc.utils as utils
+
+import riboutils.ribo_filenames as filenames
+import riboutils.ribo_utils as ribo_utils
 
 import logging
 import misc.logging_utils as logging_utils
@@ -18,40 +24,88 @@ def main():
         "by create-read-length-orf-profiles into a single sparse \"tensor\". "
         "N.B. This script is called by create-read-length-orf-profiles, and "
         "it is unlikely that it should ever be called independently.")
-
-    parser.add_argument('mtx_template', help="The template for the individual "
-        "output files. The tempalte should use ${length} and ${offset} to "
-        "indicate where the respective values should appear in the filename. "
-        "N.B. This string probably needs to be given in single quotes so that "
-        "the template variables are not interpreted by the shell.")
-
+    
+    parser.add_argument('config', help="The (json) config file")
+    parser.add_argument('name', help="The name for the dataset, used in the "
+        "created files")
+    
     parser.add_argument('out', help="The (mtx.gz) output file containing the "
         "ORF profiles and read lengths")
 
-
-    parser.add_argument('-l', '--lengths', help="If any values are given, "
-        "then only reads which have those lengths will be included in the "
-        "signal construction.", type=int, default=default_lengths, nargs='*')
-    parser.add_argument('-o', '--offsets', help="The 5' end of reads will be "
-        "shifted by this amount. There must be one offset value for each "
-        "length (given by the --lengths argument.", type=int, 
-        default=default_offsets, nargs='*')
+    parser.add_argument('-c', '--is-condition', help="If this flag is present, "
+        "then \"name\" will be taken to be a condition name. The profiles for "
+        "all relevant replicates of the condition will be created.", 
+        action='store_true')
 
     logging_utils.add_logging_options(parser)
     args = parser.parse_args()
     logging_utils.update_logging(args)
-
-    msg = "Creating mtx template"
+    
+    msg = "Reading config file"
     logger.info(msg)
-    mtx_template = string.Template(args.mtx_template)
+    config = yaml.load(open(args.config))
+
+    # pull out what we need from the config file
+    is_unique = not ('keep_riboseq_multimappers' in config)    
+    seqname_prefix_str = utils.get_config_argument(config, 'seqname_prefix')
+    note = config.get('note', None)
+
+    names = [args.name]
+    if args.is_condition:
+        riboseq_replicates = ribo_utils.get_riboseq_replicates(config)
+        names = [n for n in riboseq_replicates[args.name]]
+
+    # keep a map from the lengths to the combined profiles
+    length_profile_map = {}
+
+    for name in names:
+
+        msg = "Processing sample: {}".format(name)
+        logger.info(msg)
+        
+        # get the lengths and offsets which meet the required criteria from the config file
+        lengths, offsets = ribo_utils.get_periodic_lengths_and_offsets(
+            config, 
+            name, 
+            is_unique=is_unique
+        )
+
+        lengths = [28, 29]
+        offsets = [12, 12]
+
+        if len(lengths) == 0:
+            msg = ("No periodic read lengths and offsets were found. Try relaxing "
+                "min_metagene_profile_count, min_metagene_bf_mean, "
+                "max_metagene_bf_var, and/or min_metagene_bf_likelihood. Qutting.")
+            logger.critical(msg)
+            return
+
+        for length, offset in zip(lengths, offsets):
+                        
+            mtx = filenames.get_riboseq_profiles(
+                config['riboseq_data'], 
+                name, 
+                length=[length], 
+                offset=[offset], 
+                is_unique=is_unique, 
+                note=note
+            )
+
+            mtx = scipy.io.mmread(mtx).tocsr()
+
+            prior_mtx = length_profile_map.get(length, None)
+
+            if prior_mtx is None:
+                length_profile_map[length] = mtx
+            else:
+                length_profile_map[length] = prior_mtx + mtx
 
     with gzip.open(args.out, 'wb') as target_gz:
 
-        for length, offset in zip(args.lengths, args.offsets):
-            mtx = mtx_template.substitute(length=length, offset=offset)
-            mtx = scipy.io.mmread(mtx)
-            
-            msg = "Processing ORF profiles. length: {}. offset: {}.".format(length, offset)
+        for length, mtx in length_profile_map.items():
+            mtx = mtx.tocoo()
+                        
+            msg = "Writing ORF profiles. length: {}.".format(length)
             logger.info(msg)
             
             for row, col, val in zip(mtx.row, mtx.col, mtx.data):
--- rpbp/analysis/profile_construction/create_read_length_orf_profiles.py
+++ rpbp/analysis/profile_construction/create_read_length_orf_profiles.py
@@ -4,17 +4,17 @@ import argparse
 import shlex
 import string
 import sys
+import yaml
 
 import misc.slurm as slurm
 import misc.utils as utils
 
+import riboutils.ribo_filenames as filenames
+import riboutils.ribo_utils as ribo_utils
+
 import logging
 import misc.logging_utils as logging_utils
 logger = logging.getLogger(__name__)
-
-default_lengths = []
-default_offsets = []
-default_seqname_prefix = ""
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -22,32 +22,17 @@ def main():
         "and offset independently. One sparse matrix file will be created for "
         "each read length. It then collects the values into a sparse tensor.")
 
-    parser.add_argument('bam', help="The bam file including filtered (unique, "
-        "etc.) alignments")
-    parser.add_argument('orfs', help="The (bed12) file containing the ORFs")
-    parser.add_argument('exons', help="The (bed6+2) file containing the exons")
+    parser.add_argument('config', help="The (json) config file")
+    parser.add_argument('name', help="The name for the dataset, used in the "
+        "created files")
     
-    parser.add_argument('mtx_template', help="The template for the individual "
-        "output files. The tempalte should use ${length} and ${offset} to "
-        "indicate where the respective values should appear in the filename. "
-        "N.B. This string probably needs to be given in single quotes so that "
-        "the template variables are not interpreted by the shell.")
-
     parser.add_argument('out', help="The (mtx.gz) output file containing the "
         "ORF profiles and read lengths")
 
-
-    parser.add_argument('-l', '--lengths', help="If any values are given, "
-        "then only reads which have those lengths will be included in the "
-        "signal construction.", type=int, default=default_lengths, nargs='*')
-    parser.add_argument('-o', '--offsets', help="The 5' end of reads will be "
-        "shifted by this amount. There must be one offset value for each "
-        "length (given by the --lengths argument.", type=int, 
-        default=default_offsets, nargs='*')
-       
-    parser.add_argument('--seqname-prefix', help="If present, this string "
-        "will be prepended to the seqname field of the ORFs.", 
-        default=default_seqname_prefix)
+    parser.add_argument('-c', '--is-condition', help="If this flag is present, "
+        "then \"name\" will be taken to be a condition name. The profiles for "
+        "all relevant replicates of the condition will be created.", 
+        action='store_true')
 
     slurm.add_sbatch_options(parser)
     logging_utils.add_logging_options(parser)
@@ -55,60 +40,128 @@ def main():
     logging_utils.update_logging(args)
 
     logging_str = logging_utils.get_logging_options_string(args)
-    seqname_str = "--seqname-prefix {}".format(shlex.quote(args.seqname_prefix))
     cpus_str = "--num-cpus {}".format(args.num_cpus)
-
-    mtx_template = string.Template(args.mtx_template)
 
     msg = "[create-read-length-orf-profiles]: {}".format(' '.join(sys.argv))
     logger.info(msg)
-    
-    # make sure the number of lengths and offsets match
-    if len(args.lengths) != len(args.offsets):
-        msg = ("[create-read-length-orf-profiles]: the number of --lengths "
-            "and --offsets do not match.")
-        raise ValueError(msg)
 
+    msg = "Reading config file"
+    logger.info(msg)
+    config = yaml.load(open(args.config))
+ 
+    # pull out what we need from the config file
+    is_unique = not ('keep_riboseq_multimappers' in config)    
+    seqname_str = utils.get_config_argument(config, 'seqname_prefix')
+    note = config.get('note', None)
+    orf_note = config.get('orf_note', None)
+
+    
+    orfs = filenames.get_orfs(
+        config['genome_base_path'], 
+        config['genome_name'], 
+        note=orf_note
+    )
+
+    exons = filenames.get_exons(
+        config['genome_base_path'], 
+        config['genome_name'],
+        note=orf_note
+    )
+    
     # make sure the necessary files exist
-    required_files = [args.bam, args.orfs, args.exons]
+    required_files = [orfs, exons]
     msg = "[create-read-length-orf-profiles]: Some input files were missing: "
     utils.check_files_exist(required_files, msg=msg)
 
+    # check which samples to process
+    names = [args.name]
+    if args.is_condition:
+        riboseq_replicates = ribo_utils.get_riboseq_replicates(config)
+        names = [n for n in riboseq_replicates[args.name]]
+
     job_ids = []
-    for length, offset in zip(args.lengths, args.offsets):
-        lengths_str = "--lengths {}".format(length)
-        offsets_str = "--offsets {}".format(offset)
+    for name in names:
 
-        mtx = mtx_template.substitute(length=length, offset=offset)
-
-        cmd = "extract-orf-profiles {} {} {} {} {} {} {} {}".format(
-            args.bam,
-            args.orfs,
-            args.exons,
-            mtx,
-            lengths_str,
-            offsets_str,
-            seqname_str,
-            cpus_str,
-            logging_str
-        )
+        msg = "Processing sample: {}".format(name)
+        logger.info(msg)
         
-        job_id = slurm.check_sbatch(cmd, args=args)
+        # now the relevant files
+        bam = filenames.get_riboseq_bam(
+            config['riboseq_data'], 
+            name, 
+            is_unique=is_unique, 
+            note=note
+        )
 
-        job_ids.append(job_id)
+        # make sure the necessary files exist
+        required_files = [bam]
+        msg = "[create-read-length-orf-profiles]: Some input files were missing: "
+        utils.check_files_exist(required_files, msg=msg)
+
+        # get the lengths and offsets which meet the required criteria from the config file
+        lengths, offsets = ribo_utils.get_periodic_lengths_and_offsets(
+            config, 
+            name, 
+            is_unique=is_unique
+        )
+
+        lengths = [28, 29]
+        offsets = [12, 12]
+
+        if len(lengths) == 0:
+            msg = ("No periodic read lengths and offsets were found. Try relaxing "
+                "min_metagene_profile_count, min_metagene_bf_mean, "
+                "max_metagene_bf_var, and/or min_metagene_bf_likelihood. Qutting.")
+            logger.critical(msg)
+            return
+
+
+        for length, offset in zip(lengths, offsets):
+            lengths_str = "--lengths {}".format(length)
+            offsets_str = "--offsets {}".format(offset)
+
+            
+            mtx = filenames.get_riboseq_profiles(
+                config['riboseq_data'], 
+                name, 
+                length=[length], 
+                offset=[offset],
+                is_unique=is_unique, 
+                note=note
+            )
+
+            cmd = "extract-orf-profiles {} {} {} {} {} {} {} {}".format(
+                bam,
+                orfs,
+                exons,
+                mtx,
+                lengths_str,
+                offsets_str,
+                seqname_str,
+                cpus_str,
+                logging_str
+            )
+            
+            job_id = slurm.check_sbatch(cmd, args=args)
+
+            job_ids.append(job_id)
 
     # now, collect them into a single file
-    offsets_str = ' '.join(str(o) for o in args.offsets)
-    lengths_str = ' '.join(str(l) for l in args.lengths)
+    offsets_str = ' '.join(str(o) for o in offsets)
+    lengths_str = ' '.join(str(l) for l in lengths)
 
     offsets_str = "--offsets {}".format(offsets_str)
     lengths_str = "--lengths {}".format(lengths_str)
 
+    is_condition_str = ""
+    if args.is_condition:
+        is_condition_str = "--is-condition"
+
     cmd = "collect-read-length-orf-profiles {} {} {} {} {}".format(
-        shlex.quote(args.mtx_template),
+        args.config,
+        args.name,
         args.out,
-        offsets_str,
-        lengths_str,
+        is_condition_str,
         logging_str
     )
 
