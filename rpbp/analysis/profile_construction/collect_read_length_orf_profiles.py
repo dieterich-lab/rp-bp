3de86a7e32073730f08182d90be4dc360a327ea9
Etienne Boileau <boileau@uni-heidelberg.de>
2018-04-13 15:45:57 +0200

    UPD analysis orf counting profiles

--- rpbp/analysis/profile_construction/collect_read_length_orf_profiles.py
+++ rpbp/analysis/profile_construction/collect_read_length_orf_profiles.py
@@ -2,14 +2,13 @@
 
 import argparse
 import gzip
-import string
 import scipy.io
 import yaml
 
-import misc.utils as utils
-
 import riboutils.ribo_filenames as filenames
 import riboutils.ribo_utils as ribo_utils
+
+import bio_utils.bed_utils as bed_utils
 
 import logging
 import misc.logging_utils as logging_utils
@@ -20,22 +19,26 @@ default_offsets = []
 
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        description="Collect the individual read length ORF profiles created "
-        "by create-read-length-orf-profiles into a single sparse \"tensor\". "
-        "N.B. This script is called by create-read-length-orf-profiles, and "
-        "it is unlikely that it should ever be called independently.")
+        description="Collect the individual read length ORF profiles (mtx) created "
+        "by 'create-read-length-orf-profiles' into a single 'sparse tensor'. "
+        "N.B. This script is called by 'create-read-length-orf-profiles', however"
+        "we still call each sample independently for condition, lengths and offsets")
     
-    parser.add_argument('config', help="The (json) config file")
-    parser.add_argument('name', help="The name for the dataset, used in the "
-        "created files")
+    parser.add_argument('config', help="The (yaml) config file")
+    parser.add_argument('name', help="The name of either one of the 'riboseq_samples'"
+        "or 'riboseq_biological_replicates' from the config file.")
     
-    parser.add_argument('out', help="The (mtx.gz) output file containing the "
-        "ORF profiles and read lengths")
+    parser.add_argument('out', help="The output (txt.gz) file. N.B. The output uses"
+        "base-0 indexing, contrary to the unsmoothed ORF profiles, which are written"
+        "using the matrix market format (base-1 indexing).")
 
     parser.add_argument('-c', '--is-condition', help="If this flag is present, "
-        "then \"name\" will be taken to be a condition name. The profiles for "
+        "then 'name' will be taken to be a condition name. The profiles for "
         "all relevant replicates of the condition will be created.", 
         action='store_true')
+
+    parser.add_argument('--add-ids', help="If this flag is present, "
+        "then orf_ids will be added to the final output.", action='store_true')
 
     logging_utils.add_logging_options(parser)
     args = parser.parse_args()
@@ -46,9 +49,17 @@ def main():
     config = yaml.load(open(args.config))
 
     # pull out what we need from the config file
-    is_unique = not ('keep_riboseq_multimappers' in config)    
-    seqname_prefix_str = utils.get_config_argument(config, 'seqname_prefix')
+    is_unique = not ('keep_riboseq_multimappers' in config)
     note = config.get('note', None)
+
+    if args.add_ids:
+        orf_note = config.get('orf_note', None)
+        orfs_file = filenames.get_orfs(
+            config['genome_base_path'],
+            config['genome_name'],
+            note=orf_note
+        )
+        orfs = bed_utils.read_bed(orfs_file)
 
     names = [args.name]
     if args.is_condition:
@@ -97,17 +108,32 @@ def main():
             else:
                 length_profile_map[length] = prior_mtx + mtx
 
-    with gzip.open(args.out, 'wb') as target_gz:
+    if args.add_ids:
+        with gzip.open(args.out, 'wb') as target_gz:
 
-        for length, mtx in length_profile_map.items():
-            mtx = mtx.tocoo()
-                        
-            msg = "Writing ORF profiles. length: {}.".format(length)
-            logger.info(msg)
-            
-            for row, col, val in zip(mtx.row, mtx.col, mtx.data):
-                s = "{} {} {} {}\n".format(length, row, col, val)
-                target_gz.write(s.encode())
+            for length, mtx in length_profile_map.items():
+                mtx = mtx.tocoo()
+
+                msg = "Writing ORF profiles. length: {}.".format(length)
+                logger.info(msg)
+
+                for row, col, val in zip(mtx.row, mtx.col, mtx.data):
+                    # orf_num are both zero-based, since we are now using coo
+                    orf_id = orfs.loc[orfs['orf_num'] == row]['id']
+                    s = "{} {} {} {} {}\n".format(row, orf_id, col, length, val)
+                    target_gz.write(s.encode())
+    else:
+        with gzip.open(args.out, 'wb') as target_gz:
+
+            for length, mtx in length_profile_map.items():
+                mtx = mtx.tocoo()
+
+                msg = "Writing ORF profiles. length: {}.".format(length)
+                logger.info(msg)
+
+                for row, col, val in zip(mtx.row, mtx.col, mtx.data):
+                    s = "{} {} {} {}\n".format(row, col, length, val)
+                    target_gz.write(s.encode())
 
 if __name__ == '__main__':
     main()
--- rpbp/analysis/profile_construction/create_read_length_orf_profiles.py
+++ rpbp/analysis/profile_construction/create_read_length_orf_profiles.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python3
 
 import argparse
-import shlex
-import string
 import sys
 import yaml
 
@@ -19,20 +17,24 @@ logger = logging.getLogger(__name__)
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description="Extract the ORF profiles for each specified read length "
-        "and offset independently. One sparse matrix file will be created for "
-        "each read length. It then collects the values into a sparse tensor.")
+        "and offset independently, creating one sparse matrix file (mtx) for "
+        "each read length. These are then collected into a 'sparse tensor'.")
 
-    parser.add_argument('config', help="The (json) config file")
-    parser.add_argument('name', help="The name for the dataset, used in the "
-        "created files")
+    parser.add_argument('config', help="The yaml config file.")
+    parser.add_argument('name', help="The name of either one of the 'riboseq_samples'"
+        "or 'riboseq_biological_replicates' from the config file.")
     
-    parser.add_argument('out', help="The (mtx.gz) output file containing the "
-        "ORF profiles and read lengths")
+    parser.add_argument('out', help="The output (txt.gz) file. N.B. The output uses"
+        "base-0 indexing, contrary to the unsmoothed ORF profiles, which are written"
+        "using the matrix market format (base-1 indexing).")
 
     parser.add_argument('-c', '--is-condition', help="If this flag is present, "
-        "then \"name\" will be taken to be a condition name. The profiles for "
+        "then 'name' will be taken to be a condition name. The profiles for "
         "all relevant replicates of the condition will be created.", 
         action='store_true')
+
+    parser.add_argument('--add-ids', help="If this flag is present, "
+        "then orf_ids will be added to the final output.", action='store_true')
 
     slurm.add_sbatch_options(parser)
     logging_utils.add_logging_options(parser)
@@ -54,7 +56,6 @@ def main():
     seqname_str = utils.get_config_argument(config, 'seqname_prefix')
     note = config.get('note', None)
     orf_note = config.get('orf_note', None)
-
     
     orfs = filenames.get_orfs(
         config['genome_base_path'], 
@@ -74,9 +75,11 @@ def main():
     msg = "[create-read-length-orf-profiles]: Some input files were missing: "
     utils.check_files_exist(required_files, msg=msg)
 
-    # check which samples to process
+    # process one sample or all samples from condition
     names = [args.name]
+    is_condition_str = ""
     if args.is_condition:
+        is_condition_str = "--is-condition"
         riboseq_replicates = ribo_utils.get_riboseq_replicates(config)
         names = [n for n in riboseq_replicates[args.name]]
 
@@ -113,12 +116,10 @@ def main():
             logger.critical(msg)
             return
 
-
         for length, offset in zip(lengths, offsets):
             lengths_str = "--lengths {}".format(length)
             offsets_str = "--offsets {}".format(offset)
 
-            
             mtx = filenames.get_riboseq_profiles(
                 config['riboseq_data'], 
                 name, 
@@ -144,22 +145,16 @@ def main():
 
             job_ids.append(job_id)
 
-    # now, collect them into a single file
-    offsets_str = ' '.join(str(o) for o in offsets)
-    lengths_str = ' '.join(str(l) for l in lengths)
-
-    offsets_str = "--offsets {}".format(offsets_str)
-    lengths_str = "--lengths {}".format(lengths_str)
-
-    is_condition_str = ""
-    if args.is_condition:
-        is_condition_str = "--is-condition"
+    add_ids_str = ""
+    if args.add_ids:
+        add_ids_str = "--add-ids"
 
     cmd = "collect-read-length-orf-profiles {} {} {} {} {}".format(
         args.config,
         args.name,
         args.out,
         is_condition_str,
+        add_ids_str,
         logging_str
     )
 
