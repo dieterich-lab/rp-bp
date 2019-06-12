3cd3cb56c55a0c25494dfba7c67c4d05cdcb4d86
Etienne Boileau <boileau@uni-heidelberg.de>
2019-06-12 15:19:09 +0200

    sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- using markdown ------------------------------------------------
@@ -39,7 +39,7 @@ source_suffix = ['.rst', '.md']
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '1.6'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
