dac8851802e26b3a5d8a5dba178a89cc7654ddf2
Liam Keegan <liam@keegan.ch>
2022-08-08 14:46:55 +0200

    improve tests
    - assert equality of `readlines()` instead of hash
      - pytest then gives the diff & location if files differ
    - add filenames to the args of the raised `AssertionError`
      - they are then also displayed by pytest when the test fails
    - add to CI

--- tests/regression/test_prepare_genome.py
+++ tests/regression/test_prepare_genome.py
@@ -1,18 +1,25 @@
 
 import pytest
+import gzip
 
 
-def check_hash(h1, h2, hash_func):
-    return hash_func(h1) == hash_func(h2)
-    
-#@pytest.mark.parametrize('files, ref_files, checker_function', 
-                         #[(files, ref_files, checker_function) for files, ref_files, checker_function in get_files])
-#def test_output(files, ref_files, checker_function):
-    #assert check_hash(files, ref_files, checker_function)
-    
+def get_lines(filename):
+    if filename.endswith(".gz"):
+        with gzip.open(filename, 'rb') as f:
+            return f.readlines()
+    with open(filename, 'r') as f:
+        return f.readlines()
+
+
+def files_match(file, ref_file):
+    try:
+        assert get_lines(file) == get_lines(ref_file)
+    except AssertionError as e:
+        e.args += ("File:", file, "Reference file:", ref_file)
+        raise
+    return True
+
+
 def test_output(get_files):
-    for files, ref_files, checker_function in get_files:
-        assert check_hash(files, ref_files, checker_function)
-        
-        
-        
+    for file, ref_file in get_files:
+        assert files_match(file, ref_file)
