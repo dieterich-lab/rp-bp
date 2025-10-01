991512ab5bf689b4fd44c029052401a4fea1f69c
Etienne Boileau <boileau@uni-heidelberg.de>
2025-10-01 12:14:58 +0200

    WIP CI for macos

--- src/rpbp/defaults.py
+++ src/rpbp/defaults.py
@@ -42,7 +42,7 @@ default_stop_codons = ["TAA", "TGA", "TAG"]
 
 default_read_files_command = "zcat"
 if sys.platform.startswith("darwin"):
-    default_read_files_command = "gunzip -c"
+    default_read_files_command = "gzcat"
 
 # NOTES:
 #   --limitBAMsortRAM 0 (with --genomeLoad NoSharedMemory)
